8a38db9322d09798911a43eba4cc990857cd4bae
meziani2020 <issine14@gmail.com>
2020-11-12 15:33:09 -0700

    test amazon multilangue functionality for german verssion

--- src/test/java/steps_definition/MultLanguTestStepdefs.java
+++ src/test/java/steps_definition/MultLanguTestStepdefs.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,12 +16,7 @@ public class MultLanguTestStepdefs {
     WebDriver driver;
     Actions action ;
     WebElement we;
-    String weId="";
-    String weValue="";
-    String weClassName="";
     String PageTitle="";
-    //String url="https://www.google.com";
-    String  url="https://www.google.com/?hl=eN&gl=us";
     Boolean goodCondition=false;
 
 
@@ -47,8 +43,11 @@ public class MultLanguTestStepdefs {
 
     @And("user click on element determined by the id{string}")
     public void userClickOnLinkElementDeterminedByTheId(String elemId) {
+
         we=driver.findElement(By.id(elemId));
-        goodCondition=we!=null;
+
+        goodCondition=we!=null && we.isDisplayed() ;
+        System.out.println("goodCondition="+goodCondition);
         we.click();
         Assert.assertTrue(goodCondition);
 
@@ -67,23 +66,34 @@ public class MultLanguTestStepdefs {
     @And("user sould land in a page title contains {string}")
     public void userSouldLandInAPageWhereTitledContains(String textInTitle) {
         PageTitle=driver.getTitle();
+        System.out.println("PageTitle=="+PageTitle);
         goodCondition=PageTitle.contains(textInTitle);
         Assert.assertTrue(goodCondition);
     }
 
     @And("user white tell element identefied by {string} sows up")
     public void userWhiteTellElementIdentefiedBySowsUp(String elemId) {
-        we=driver.findElement(By.id(elemId));
+        //we=driver.findElement(By.id(elemId));
+        we=null;
+        goodCondition=false;
+        int i=0;
+        while (  (we==null || !we.isDisplayed()) &&  i<5 ) {
 
-        while (we==null || !we.isDisplayed() ) {
             try {
-                driver.wait(1000);
+                //driver.wait(1000);
+                //driver.manage().sl;
+                Thread.sleep(1000);
+                we=driver.findElement(By.id(elemId));
+
             } catch (InterruptedException e) {
                 goodCondition=false;
             }
-            goodCondition=we.isDisplayed();
-            Assert.assertTrue(goodCondition);
+            i++;
+            System.out.println( "wait...");
+
         }
+           goodCondition=we.isDisplayed();
+           Assert.assertTrue(goodCondition);
         }
 
     @And("user click on element determined by the xpath{string}")
@@ -98,10 +108,6 @@ public class MultLanguTestStepdefs {
 
 
 
-    @And("user click on tagElement \\{string} having as content\\{string}")
-    public void userClickOnTagElementStringHavingAsContentString() {
-
-    }
 
     @And("user click on tagElement {string} having as content{string}")
     public void userClickOnTagElementHavingAsContent(String tag, String txt) {
@@ -111,5 +117,53 @@ public class MultLanguTestStepdefs {
         we=listE.get(0);
         we.click();
         Assert.assertTrue(goodCondition);
+    }
+
+
+    @And("user click on tagElement {string} contaning in {string}")
+    public void userClickOnTagElementContaningIn(String tag, String txt) {
+        //String pth="//"+tag+"[text()='"+txt+"']";
+        String pth="//"+tag+"[contains(text(),'"+txt+"')]"; //span[contains(text(),'Go to website')]
+        List<WebElement> listE=driver.findElements(By.xpath(pth));
+        goodCondition= listE.size()==1;
+        we=listE.get(0);
+        we.click();
+        Assert.assertTrue(goodCondition);
+    }
+
+    @And("user click on  parent of  tagElement {string} contaning in {string}")
+    public void userClickOnParentOfTagElementContaningIn(String tag, String txt) {
+
+        String pth="//"+tag+"[contains(text(),'"+txt+"')]"; //span[contains(text(),'Go to website')]
+        List<WebElement> listE=driver.findElements(By.xpath(pth));
+        goodCondition= listE.size()==1;
+        we=listE.get(0);
+        WebElement parent=we.findElement(By.xpath(".."));
+        parent.click();
+        Assert.assertTrue(goodCondition);
+    }
+
+    @And("user sould land in new tab were page title contains {string}")
+    public void userSouldLandInNewTabWerePageTitleContains(String textInTitle) {
+
+        //t
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL +"9");
+
+        String w0=driver.getWindowHandle();
+        String w0a=(String) driver.getWindowHandles().toArray()[0];
+        String w1= (String) driver.getWindowHandles().toArray()[1];
+        System.out.println(driver.getWindowHandles().size());
+        System.out.println(w0);
+        System.out.println(w0a);
+        System.out.println(w1);
+        PageTitle=driver.getTitle();
+        System.out.println("PageTitle befor switch=="+PageTitle);
+
+        driver.switchTo().window(w1);
+        PageTitle=driver.getTitle();
+        System.out.println("PageTitle after switch=="+PageTitle);
+        goodCondition=PageTitle.contains(textInTitle);
+        Assert.assertTrue(goodCondition);
+
     }
 }
--- src/test/resources/features/AmazoneTest02.feature
+++ src/test/resources/features/AmazoneTest02.feature
@@ -1,28 +1,27 @@
 Feature: Multi lang test
-  Scenario: testing if a link L1 lands in page P1
+  Scenario: testing if Germany (Deutschland)  version is qccesible
     Given  use open page in the url "https://www.amazon.com/"
     #And   the page Title should containe "Amazon.com: Online Shopping"
     And   user sould land in a page title contains "Amazon.com: Online Shopping"
     # link: multilangues flag.
     And   use go over the element determined by id "icp-nav-flyout"
-    And  user white tell element identefied by "nav-flyout-icp" sows up
+    #And  user white tell element identefied by "nav-flyout-icp" sows up
+    And  user white tell element identefied by "icp-flyout-mkt-change" sows up
     # link: Change country/region.
     And   user click on element determined by the id"icp-flyout-mkt-change"
     And   user sould land in a page title contains "Go to website"
-    #
-    And   user click on element determined by the id"icp-selected-country"
-    And   user click on tagElement "span" having as content"France"
+    #    icp-selected-country
+    And   user click on element determined by the id"a-autoid-0-announce"
+    #And   user click on tagElement "span" having as content"France"
+    And   user click on tagElement "span" having as content"Germany (Deutschland)"
     #And   user click on element determined by the xpath"//span[text()='France']"
-    And   user click on tagElement "span" having as content"France"
-
+    #And  user click on tagElement "span" having as content"Go to website"
+    #And user click on  tagElement "span" contaning in "Go to website"
+    And user click on  parent of  tagElement "span" contaning in "Go to website"
+    And   user sould land in new tab were page title contains "Amazon.de"
 
 
     # test if multilanguages functions are working corrctely
     # use open page in the url "https://www.amazon.com/"
-    #
-    #
-    #
-    #
-    #
     #
     #
